339e03d18c67515019d48b4615adf154d254795f
jesuseladiom <jesuseladiom@gmail.com>
2025-02-20 13:36:57 -0700

    Agregando un Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
+  
   <div class="d-flex">
     <Sidebar />
-    <div class="flex-grow-1 p-3" style="background-color: #f8f9fa; min-height: 100vh;">
-      <router-view />
+    <div class="flex-grow-1 d-flex flex-column">
+      <Navbar />
+      <div class="p-3" style="background-color: #f8f9fa; min-height: 100vh;">
+        <router-view />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 import Sidebar from './components/Sidebar.vue';
+import Navbar from './components/Navbar.vue';
 </script>
 
